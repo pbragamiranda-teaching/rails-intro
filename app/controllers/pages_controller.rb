22418aa6a22f4c78d5b99b8cc043d6eeccf39d93
pbragamiranda <pbragamiranda@gmail.com>
2022-08-15 10:56:27 +0200

    first rails app

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,23 @@
+class PagesController < ApplicationController
+  def about
+  end
+
+  def contact
+    search = params[:member]
+    @members = ['Ana', 'Ruben', 'Dan', 'Esther', 'Marie', 'Rubens']
+
+    # if search.nil?
+    #   @members
+    # else
+    #   # Rub  -> ruben and rubens
+    #   @members = @members.select do |member|
+    #     member.start_with?(search)
+    #   end
+    # end
+
+    if search
+      @members = @members.select { |member| member.start_with?(search) }
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # VERB url_path, to: 'controller#method'
+  get 'about', to: 'pages#about', as: :about
+  get 'contact', to: 'pages#contact', as: :contact
 end
